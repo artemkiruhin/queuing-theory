efbd1128fdab3933113f4e5dde26577d643fbfb1
artemkiruhin <temkaunik@gmail.com>
2025-02-09 22:42:32 +0300

    CORS configuration

--- QueuingTheory.Api/Program.cs
+++ QueuingTheory.Api/Program.cs
@@ -3,10 +3,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
