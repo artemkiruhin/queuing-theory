7a448810c6dc3ef6f3995bc049d8ee39832b1ca6
artemkiruhin <temkaunik@gmail.com>
2025-02-09 22:33:31 +0300

    Models created

---
+++ QueuingTheory.Api/Models/QueueingModel.cs
@@ -0,0 +1,8 @@
+﻿namespace QueuingTheory.Api.Models;
+
+public class QueueingModel
+{
+    public double ArrivalRate { get; set; }          // λ (lambda)
+    public double ServiceRate { get; set; }          // μ (mu)
+    public int Servers { get; set; } = 1;            // c (number of servers, 1 for M/M/1)
+}
---
+++ QueuingTheory.Api/Models/QueueingResults.cs
@@ -0,0 +1,11 @@
+﻿namespace QueuingTheory.Api.Models;
+
+public class QueueingResults
+{
+    public double Utilization { get; set; }          // ρ (rho)
+    public double L { get; set; }                    // Average number in system
+    public double Lq { get; set; }                   // Average number in queue
+    public double W { get; set; }                    // Average time in system
+    public double Wq { get; set; }                   // Average time in queue
+    public double P0 { get; set; }                   // Probability of empty system
+}
